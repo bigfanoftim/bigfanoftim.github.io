2a8dfaa58c188e857456d3ac78cc16ee0670f4fe
bigfanoftim <bigfanoftim@gmail.com>
2024-03-13 23:18:58 +0900

    prefix 추가

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 const { withContentlayer } = require("next-contentlayer");
 
+const prefix = process.env.NODE_ENV === 'production' ? 'https://bigfanoftim.github.io/' : ''
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  output: "export"
+  output: "export",
+  assetPrefix: prefix
 };
 
 module.exports = withContentlayer(nextConfig);
